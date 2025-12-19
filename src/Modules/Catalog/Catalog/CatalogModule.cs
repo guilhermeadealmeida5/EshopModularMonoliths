342290db6fbad24a435d9620f420698d9036ecee
Guilherme Almeida - TI - Plataformas Digitais <guilherme.almeida@editoradobrasil.com.br>
2025-12-19 15:33:26 -0300

    First commit

---
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog
+{
+    public static class CatalogModule
+    {
+        public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Add services to the container.
+            //services
+            //    .AddApplicationServices()
+            //    .AddInfrastructureServices(configuration)
+            //    .AddApiServices(configuration);
+
+            return services;
+        }
+    }
+}
